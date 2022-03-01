7e1ee68e64173b397367d63e05db0643cf81b730
Ian <iansutherland.az@gmail.com>
2022-03-01 07:04:18 -0700

    added layer control & got rid of mapbox tileset in favor of openstreetmap

--- src/basicMap.tsx
+++ src/basicMap.tsx
@@ -1,11 +1,10 @@
-import { MapContainer, Popup } from 'react-leaflet';
+import { LayersControl, MapContainer, Popup, TileLayer } from 'react-leaflet';
 import "leaflet/dist/leaflet.css";
 import L from 'leaflet';
 import PhoenixDataService from './services/PhoenixDataService';
 import React, { useState, useEffect } from 'react';
 import WifiSite from './models/WifiSite';
 import Pin from './components/Pin';
-import LayerControl from './components/layer/LayerControl';
 import MapBoxUrl, { mapBoxAttribution} from './services/MapBoxService';
 
 export default function BasicMap()
@@ -16,7 +15,7 @@ export default function BasicMap()
         Light: L.tileLayer(MapBoxUrl(), {id: 'mapbox/light-v10', tileSize: 512, zoomOffset: -1, attribution: mapBoxAttribution}),
         Satellite: L.tileLayer(MapBoxUrl(), {id: 'mapbox/satellite-v9', tileSize: 512, zoomOffset: -1, attribution: mapBoxAttribution}),
     }
-    const [baseMap, setBaseMap] = useState(baseMaps.Satellite);
+    const attributionText: string = 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
     const phoenixDataService = new PhoenixDataService();
 
     useEffect(() => {
@@ -28,7 +27,26 @@ export default function BasicMap()
     }, []);
     
     return(
-        <MapContainer id='map' center={[33.448376, -112.074036]} zoom={13} layers={[baseMap]}>
+        <MapContainer id='map' center={[33.448376, -112.074036]} zoom={13}>
+            <LayersControl position="topright">
+                <LayersControl.BaseLayer name='Streets'>
+                    <TileLayer
+                        attribution={attributionText}
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                </LayersControl.BaseLayer>
+                <LayersControl.BaseLayer checked name='Humanitarian'>
+                    <TileLayer 
+                        attribution={attributionText}
+                        url="https://tile-a.openstreetmap.fr/hot/{z}/{x}/{y}.png"/>
+                </LayersControl.BaseLayer>
+                <LayersControl.BaseLayer name="Cyclosm">
+                    <TileLayer 
+                        attribution={attributionText}
+                        url = 'https://a.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png'/>
+                </LayersControl.BaseLayer>
+
+            </LayersControl>
         {data ? data.map((spot: WifiSite) => {
             return(
                 <Pin key={spot._id} X={spot.X} Y={spot.Y}>
